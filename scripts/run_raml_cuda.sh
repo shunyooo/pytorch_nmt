4b155ae3c244eff3fec78e7b2eb276ee1ddffe4e
shunyooo <syunyo1028@yahoo.co.jp>
2018-12-07 02:38:22 +0900

    add raml dev_limit

--- scripts/run_raml_cuda.sh
+++ scripts/run_raml_cuda.sh
@@ -50,6 +50,7 @@ python3 -u\
     --validation_log_file ${validation_log_file} \
     --valid_niter 10000 \
     --log_data_file ${log_data_file} \
+    --dev_limit 100 \
     --cuda
     #    -m ipdb nmt.py \
     # --dropout 0.2 \
